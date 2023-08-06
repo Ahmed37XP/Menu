69226315d638f52a4f45a950edf1bf5bae324ac8
Ahmed37XP <139822399+Ahmed37XP@users.noreply.github.com>
2023-08-07 00:33:41 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,125 @@
+let langBtn = document.getElementById("btn-lang");
+let otherLang = document.getElementById("other-lang");
+let back = document.getElementById("back");
+
+
+
+langBtn.addEventListener("click" , () => {
+    otherLang.style.display = "block"
+    
+})
+back.addEventListener("click" , () => {
+    otherLang.style.display = "none"
+    
+})
+
+
+let showmore = document.querySelector(".showmore");
+let x = document.querySelector(".back-btn");
+let item = document.querySelector(".items");
+let itemLabel = document.querySelector(".labels")
+
+
+showmore.addEventListener( "click" , () => {
+    item.style.display = "block";
+    itemLabel.style.display = "none";
+    showmore.style.display ="none"
+    
+
+})
+
+x.addEventListener("click" , () => {
+    item.style.display = "none";
+    itemLabel.style.display = "inline";
+    showmore.style.display ="inline"
+})
+
+
+
+
+
+let showmore1 = document.querySelector(".showmore1");
+let x1 = document.querySelector(".back-btn1");
+let item1 = document.querySelector(".items1");
+let itemLabel1 = document.querySelector(".labels1")
+
+
+showmore1.addEventListener( "click" , () => {
+    item1.style.display = "block";
+    itemLabel1.style.display = "none";
+    showmore1.style.display ="none"
+
+})
+
+x1.addEventListener("click" , () => {
+    item1.style.display = "none";
+    itemLabel1.style.display = "inline";
+    showmore1.style.display ="inline"
+})
+
+
+
+
+let showmore2 = document.querySelector(".showmore2");
+let x2 = document.querySelector(".back-btn2");
+let item2 = document.querySelector(".items2");
+let itemLabel2 = document.querySelector(".labels2")
+
+
+showmore2.addEventListener( "click" , () => {
+    item2.style.display = "block";
+    itemLabel2.style.display = "none";
+    showmore2.style.display ="none"
+
+})
+
+x2.addEventListener("click" , () => {
+    item2.style.display = "none";
+    itemLabel2.style.display = "inline";
+    showmore2.style.display ="inline"
+})
+
+
+
+let showmore3 = document.querySelector(".showmore3");
+let x3 = document.querySelector(".back-btn3");
+let item3 = document.querySelector(".items3");
+let itemLabel3 = document.querySelector(".labels3")
+
+
+showmore3.addEventListener( "click" , () => {
+    item3.style.display = "block";
+    itemLabel3.style.display = "none";
+    showmore3.style.display ="none"
+
+})
+
+x3.addEventListener("click" , () => {
+    item3.style.display = "none";
+    itemLabel3.style.display = "inline";
+    showmore3.style.display ="inline"
+})
+
+
+
+
+let showmore4 = document.querySelector(".showmore4");
+let x4 = document.querySelector(".back-btn4");
+let item4 = document.querySelector(".items4");
+let itemLabel4 = document.querySelector(".labels4")
+
+
+showmore4.addEventListener( "click" , () => {
+    item4.style.display = "block";
+    itemLabel4.style.display = "none";
+    showmore4.style.display ="none"
+
+})
+
+x4.addEventListener("click" , () => {
+    item4.style.display = "none";
+    itemLabel4.style.display = "inline";
+    showmore4.style.display ="inline"
+})
+
+
